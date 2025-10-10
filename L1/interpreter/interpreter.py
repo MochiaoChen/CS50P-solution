cb573ee275103e31c47b16b75b12842764ce471e
MochiaoChen <mochiaochen@gmail.com>
2025-10-10 23:02:49 +0800

    prettier

--- L1/interpreter/interpreter.py
+++ L1/interpreter/interpreter.py
@@ -1,12 +1,12 @@
 def main():
-    x,y,z=input("Expression:").split()
-    x,z = int(x),int(z)
+    x, y, z = input("Expression:").split()
+    x, z = int(x), int(z)
     if y == "/" and z == 0:
         print("Error: Division by zero!")
     else:
         print(f"{calculate(x, y, z):.1f}")
 
-def calculate(a,op,b):
+def calculate(a, op, b):
     if op == "+":
         return a + b
     elif op == "-":
